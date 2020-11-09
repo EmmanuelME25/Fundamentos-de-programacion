eacd767e5d7af4e558248ff63777fd8e9332c26b
EmmanuelME25 <52678688+EmmanuelME25@users.noreply.github.com>
2020-11-09 10:50:50 -0600

    Actualización un proyecto
    Incluye tareas

---
+++ Fundamentos de programacion/Tarea_01.c
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+double x=0,y=0,z=0;
+
+
+x=(6.18/5)*((14.1-4)/2)*((14.1-4)/2);
+
+y=(8.9*34)/17;
+
+z=8-7;
+
+printf("%f\n",x);
+printf("%f\n",y);
+printf("%f\n",z);
+
+    return (EXIT_SUCCESS);
+}
